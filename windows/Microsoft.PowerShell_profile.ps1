9dbe52ccd57b60cadae70686b3b4d2143cb36c25
Lekan <lwang@palantir.com>
2013-12-26 23:13:49 -0800

    Adding ga (git add), c (cd C:\)

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,16 @@
-function get-gitstatus {
+function Git-GitStatus {
 	git status
 }
 
+function Git-GitAdd {
+	git add $args
+}
+
+function Set-Location-To-C {
+	cd C:\
+}
+
 set-alias subl "C:\Program Files\Sublime Text 2\sublime_text.exe"
-set-alias gs get-gitstatus
+set-alias gs Git-GitStatus
+set-alias ga Git-GitAdd
+set-alias c Set-Location-To-C
